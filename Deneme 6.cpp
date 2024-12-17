adf0c248cb68ddd05a5f25b18080916f377f3181
beratben <166662973+beratben@users.noreply.github.com>
2024-12-17 20:19:05 +0300

    Add files via upload

---
+++ Deneme 6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+	int sifre = 1234;
+
+	int kullaniciBilgisi;
+	
+	
+
+	cout << "Kullanici sifresini giriniz: \n";
+	cin >> kullaniciBilgisi;
+
+
+	if (kullaniciBilgisi==sifre) 
+	{
+		cout << "Giris Yapildi \n";
+	}
+	else
+	{
+		cout << "Giris Basarisiz!! \n";
+
+	}
+
+	return 0;
+}
+	
+	
