d694d3bdfd2dae8d13abc57cea5e35b3c5b1d158
beratben <166662973+beratben@users.noreply.github.com>
2025-01-04 17:08:53 +0300

    Create 0dangirilenSayi.cpp
    This C++ program prompts the user to enter a number and prints the numbers from 1 to the entered number using a do-while loop. The loop ensures that the block of code is executed at least once, even if the condition is false.
    
    Key concepts demonstrated in this program:
    
    User input using cin
    do-while loop structure
    Increment operation (i++)
    Console output using cout
    //////////////////////////////////////////////////////////////////////////
    Bu C++ programı, kullanıcıdan bir sayı girmesini ister ve girilen sayıya kadar olan sayıları do-while döngüsü kullanarak ekrana yazdırır. Bu döngü türü, koşul başta sağlanmasa bile kod bloğunun en az bir kez çalıştırılmasını garanti eder.
    
    Programın temel özellikleri:
    
    Kullanıcıdan giriş alma (cin)
    do-while döngüsü yapısı
    Artırma işlemi (i++)
    Ekrana çıktı verme (cout)

---
+++ 0dangirilenSayi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int sayi, i = 0;
+
+	cout << "Bir sayi giriniz";
+	cin >> sayi;
+	do {
+		i++;
+		cout << i << endl;
+
+	}
+
+	while (i < sayi);
+	cout << endl;
+}
