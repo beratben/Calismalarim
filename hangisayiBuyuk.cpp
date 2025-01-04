792b967a61094a8395213c59e35940eb87da260e
beratben <166662973+beratben@users.noreply.github.com>
2025-01-04 16:43:41 +0300

    Create hangisayiBuyuk.cpp

---
+++ hangisayiBuyuk.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayi1, sayi2;
+	cout << "Birinci sayıyı giriniz:";
+	cin >> sayi1;
+	cout << "Ikinci sayiyi giriniz:";
+	cin >> sayi2;
+	if (sayi1 < sayi2) {
+		cout << "Ilk girilen sayi buyuktur." << endl;
+
+	}
+	else
+		cout << "Ikinci girilen sayi buyuktur." << endl;
+
+}
